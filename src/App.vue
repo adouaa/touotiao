8ef7a22ca390db8bf9228bc6f3b093bc7ed8cacd
郑英杰 <1041137820@qq.com>
2023-03-31 10:48:43 +0800

    初始化-iconfont的引入

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="app">
     <h2>黑马头条</h2>
+    <div class="toutiao toutiao-shouye"></div>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
