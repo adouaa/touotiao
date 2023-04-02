4749ec2d32a3621916a423390f92a52f5cab32bc
郑英杰 <1041137820@qq.com>
2023-04-02 17:43:15 +0800

    头部导航栏的完成

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="home">
-    <h2>home</h2>
+    <van-nav-bar class="page-nav-bar">
+      <template #title>
+        <van-button round class="search-btn" size="mini" icon="search"> 搜索 </van-button>
+      </template>
+    </van-nav-bar>
   </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
 
 <style lang="less" scoped>
-
+.home {
+  :deep(.van-nav-bar__title) {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    font-size: 28px;
+    color: #fff;
+    border: none;
+    background-color: #5babfb;
+  }
+}
 </style>
