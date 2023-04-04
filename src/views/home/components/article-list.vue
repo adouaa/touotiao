0f18cc747962ca3eb523e239c1c9e51c351817ff
郑英杰 <1041137820@qq.com>
2023-04-04 18:27:41 +0800

    用vant处理请求失败

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -4,6 +4,8 @@
       v-model:loading="loading"
       :finished="finished"
       finished-text="没有更多了"
+      v-model:error="error"
+      error-text="请求失败，点击重新加载"
       @load="onLoad"
     >
       <van-cell v-for="item in list" :key="item" :title="item.title" />
@@ -26,20 +28,32 @@ const list = ref([])
 const loading = ref(false)
 const finished = ref(false)
 const timestamp = ref(Date.now())
+const error = ref(false)
 
 const onLoad = async () => {
-  console.log('请求中')
-  const { data } = await getArticles({
-    channel_id: props.id,
-    timestamp: timestamp.value
-  })
-  if (data.results.length) {
-    console.log(timestamp.value)
-    list.value.push(...data.results)
-    timestamp.value = data.pre_timestamp
+  try {
+    const { data } = await getArticles({
+      channel_id: props.id,
+      timestamp: timestamp.value
+    })
+
+    // 模拟失败，记得写到 try-catch了里面，不然不会被 catch 捕捉并执行里面的逻辑
+    // if (Math.random() > 0.5) {
+    //   JSON.parse('d2ds')
+    // }
+
+    if (data.results.length) {
+      console.log(timestamp.value)
+      list.value.push(...data.results)
+      timestamp.value = data.pre_timestamp
+      loading.value = false
+    } else {
+      finished.value = true
+    }
+  } catch (err) {
+    // 捕捉到错误，记得要把 loading 状态置为 false，否则户一致转圈圈！
     loading.value = false
-  } else {
-    finished.value = true
+    error.value = true
   }
 }
 </script>
