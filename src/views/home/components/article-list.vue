9405f5660621c92e293aa641738a702786afa9d1
郑英杰 <1041137820@qq.com>
2023-04-04 18:51:48 +0800

    实现下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="article-list">
-    <van-list
-      v-model:loading="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      v-model:error="error"
-      error-text="请求失败，点击重新加载"
-      @load="onLoad"
+    <van-pull-refresh
+      :success-text="refreshSuccessText"
+      v-model="isRefreshLoading"
+      @refresh="onRefresh"
     >
-      <van-cell v-for="item in list" :key="item" :title="item.title" />
-    </van-list>
+      <van-list
+        v-model:loading="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        v-model:error="error"
+        error-text="请求失败，点击重新加载"
+        @load="onLoad"
+      >
+        <van-cell v-for="item in list" :key="item" :title="item.title" />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { getArticles } from '@/api/article'
+import { showToast } from 'vant'
 
 const props = defineProps({
   id: {
@@ -29,6 +36,8 @@ const loading = ref(false)
 const finished = ref(false)
 const timestamp = ref(Date.now())
 const error = ref(false)
+const isRefreshLoading = ref(false)
+const refreshSuccessText = ref('刷新成功')
 
 const onLoad = async () => {
   try {
@@ -53,8 +62,32 @@ const onLoad = async () => {
   } catch (err) {
     // 捕捉到错误，记得要把 loading 状态置为 false，否则户一致转圈圈！
     loading.value = false
+    // 展示错误提示状态
     error.value = true
   }
+}
+
+const onRefresh = async () => {
+  try {
+    timestamp.value = Date.now()
+    const { data } = await getArticles({
+      channel_id: props.id,
+      timestamp: timestamp.value
+    })
+    if (Math.random() > 0.5) {
+      JSON.parse('d2ds')
+    }
+
+    // 将数据追加到列表的 顶部
+    list.value.unshift(...data.results)
+    refreshSuccessText.value = '刷新成功'
+    showToast(`刷新成功,加载了${data.results.length}条数据`)
+  } catch (error) {
+    refreshSuccessText.value = '刷新失败，请重试'
+    showToast('刷新失败，请重试')
+  }
+  // 不管刷新成功还是失败，都要在最后把刷新的状态置为 false
+  isRefreshLoading.value = false
 }
 </script>
 
