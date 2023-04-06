64efec75d8db414fbd57aa0893b2a03d931b39ad
郑英杰 <1041137820@qq.com>
2023-04-06 15:50:47 +0800

    样式调整

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -3,22 +3,30 @@
     <van-cell>
       <template #title>
         <div style="display: flex; justify-content: space-between; align-items: center">
-          <span>我的频道</span>
-          <van-button style="padding: 0 10px" type="warning" size="mini" plain round
+          <span class="title-text">我的频道</span>
+          <van-button
+            class="edit-btn"
+            style="padding: 0 10px"
+            type="warning"
+            size="mini"
+            plain
+            round
             >编辑</van-button
           >
         </div>
       </template>
     </van-cell>
-    <van-grid :gutter="10">
-      <van-grid-item v-for="value in 8" :key="value" text="文字" />
+    <van-grid class="my-grid" :gutter="10">
+      <van-grid-item icon="clear" class="grid-item" v-for="value in 8" :key="value" text="文字" />
     </van-grid>
 
     <van-cell>
-      <template #title> 频道推荐 </template>
+      <template #title>
+        <span class="title-text">推荐频道</span>
+      </template>
     </van-cell>
-    <van-grid :gutter="10">
-      <van-grid-item v-for="value in 8" :key="value" text="文字" />
+    <van-grid class="recommend-grid" :gutter="10">
+      <van-grid-item icon="plus" class="grid-item" v-for="value in 8" :key="value" text="文字" />
     </van-grid>
   </div>
 </template>
@@ -28,5 +36,55 @@
 <style lang="less" scoped>
 .channel-edit {
   padding: 85px 0;
+
+  .title-text {
+    font-size: 32px;
+    color: #333;
+  }
+
+  .edit-btn {
+    width: 104px;
+    height: 48px;
+    font-size: 26px;
+    color: #f85959;
+  }
+
+  :deep(.grid-item) {
+    width: 160px;
+    height: 86px;
+    .van-grid-item__content {
+      background-color: #f4f5f6;
+    }
+
+    .van-grid-item__text {
+      font-size: 28px;
+      color: #222;
+    }
+  }
+
+  :deep(.recommend-grid) {
+    .van-grid-item__content {
+      flex-direction: row;
+      .van-grid-item__text {
+        margin-top: 0;
+      }
+
+      .van-grid-item__icon {
+        font-size: 38px;
+        margin-right: 6px;
+      }
+    }
+  }
+
+  :deep(.my-grid) {
+    .van-grid-item__icon {
+      position: absolute;
+      top: -10px;
+      right: -10px;
+      font-size: 30px;
+      color: #cacaca;
+      z-index: 2;
+    }
+  }
 }
 </style>
